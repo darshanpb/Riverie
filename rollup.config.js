73ffe43fb4e2fe822a85a81a9ee247280f0736c7
DARSHAN P B <darshanpb111@gmail.com>
2022-02-12 16:34:58 +0530

    button added

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,45 @@
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import typescript from "rollup-plugin-typescript2";
 import babel from "@rollup/plugin-babel";
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
 import { terser } from "rollup-plugin-terser";
 import postcss from "rollup-plugin-postcss";
+import url from "rollup-plugin-url";
 import svg from "rollup-plugin-svg";
-const INPUT_ENTRY = "src/index.tsx";
+
+const packageJson = require("./package.json");
+const INPUT_ENTRY = "src/index.ts";
 
 const ROLLUP_CONFIG = {
   input: INPUT_ENTRY,
-  output: {
-    file: "dist/index.es.js",
-    format: "es",
-  },
+  output: [
+    {
+      file: packageJson.main,
+      format: "cjs",
+      sourcemap: true,
+    },
+    {
+      file: packageJson.module,
+      format: "esm",
+      sourcemap: true,
+    },
+  ],
   plugins: [
-    typescript(),
+    peerDepsExternal(),
+    typescript({
+      useTsconfigDeclarationDir: true,
+    }),
+    resolve(),
+    commonjs(),
     babel({
       extensions: [".ts", ".tsx"],
+      exclude: "node_modules/**",
     }),
-    postcss(),
+    postcss({
+      extensions: [".css"],
+    }),
+    url(),
     svg(),
     terser(),
   ],
